ed16e8e88c2509d6ad31463e979c0717aea07876
Jordan Michele <jordanmichele32@gmail.com>
2019-01-20 12:28:26 -0500

    fixed temp bug, added trunc

--- js/index.js
+++ js/index.js
@@ -51,39 +51,42 @@
           let weatherType = data.weather[0].description;
           let kelvin = data.main.temp;
           let city = data.name;
-          fTemp = (kelvin*(9/5)-459.67).toFixed(1)+'°F';
+          fTemp = (kelvin*(9/5)-459.67);
+          //fTemp = (kelvin*(9/5)-459.67).toFixed(1)+'°F';
+          let myTrunc = Math.trunc( fTemp );
+          console.log(myTrunc);
 
-          let Rohrbach = 'At ' + (fTemp) +' pay a visit to ' + '<a href="https://www.rohrbachs.com/" target="_blank">Rohrbach Brewing Company!</a>' + ' Cozy up with a pint and a fresh homemade brick oven pizza.';
-          let Genesee = 'At ' + (fTemp) +' The ' + '<a href="https://www.geneseebeer.com/brewhouse/" target="_blank">Genesee Brew House</a>' + ' is a MUST. Enjoy this weather with a pint on their gorgeous rooftop';
-          let threeHeads = 'At ' + (fTemp) +' Visit ' + '<a href="https://threeheadsbrewing.com/" target="_blank">Three Heads Brewing.</a>' + ' Check out their Beer Garden and patio that rivals any other in the city';
-          let lock32 = 'At ' + (fTemp) +' Visit ' + '<a href="https://lock32brew.com/" target="_blank">Lock 32 Brewing Co.</a>' + ' Share some of their fresh popped popcorn, grab a cold pint and relax.';
-          let swiftWater = 'At ' + (fTemp) +' Swing by ' + '<a href="https://swiftwaterbrewing.com/" target="_blank">Swiftwater Brewing Co.</a>' + ' You can enjoy a pint inside or out with their unique garage door leading to their patio';
-          let RocBrewing = 'At ' + (fTemp) +' Stop by ' + '<a href="https://rocbrewingco.com/" target="_blank">Roc Brewing Co.</a>' + ' Grab a pint of and head out to their patio, Cheers!';
-          let lostBorough = 'At ' + (fTemp) +' Check out ' + '<a href="http://www.lostboroughbrewing.com/index.html" target="_blank">The Lost Borough Brewing Co.</a>' + ' Sticky Lips BBQ is right next door. Grab a pint, some BBQ and relax while, Cheers!';
+          let Rohrbach = 'At ' + (myTrunc) + '°F' + ' pay a visit to ' + '<a href="https://www.rohrbachs.com/" target="_blank">Rohrbach Brewing Company!</a>' + ' Cozy up with a pint and a fresh homemade brick oven pizza.';
+          let Genesee = 'At ' + (myTrunc) + '°F' + ' The ' + '<a href="https://www.geneseebeer.com/brewhouse/" target="_blank">Genesee Brew House</a>' + ' is a MUST. Enjoy this weather with a pint on their gorgeous rooftop';
+          let threeHeads = 'At ' + (myTrunc) + '°F' + ' Visit ' + '<a href="https://threeheadsbrewing.com/" target="_blank">Three Heads Brewing.</a>' + ' Check out their Beer Garden and patio that rivals any other in the city';
+          let lock32 = 'At ' + (myTrunc) + '°F' + ' Visit ' + '<a href="https://lock32brew.com/" target="_blank">Lock 32 Brewing Co.</a>' + ' Share some of their fresh popped popcorn, grab a cold pint and relax.';
+          let swiftWater = 'At ' + (myTrunc) + '°F' + ' Swing by ' + '<a href="https://swiftwaterbrewing.com/" target="_blank">Swiftwater Brewing Co.</a>' + ' You can enjoy a pint inside or out with their unique garage door leading to their patio';
+          let RocBrewing = 'At ' + (myTrunc) + '°F' + ' Stop by ' + '<a href="https://rocbrewingco.com/" target="_blank">Roc Brewing Co.</a>' + ' Grab a pint of and head out to their patio, Cheers!';
+          let lostBorough = 'At ' + (myTrunc) + '°F' + ' Check out ' + '<a href="http://www.lostboroughbrewing.com/index.html" target="_blank">The Lost Borough Brewing Co.</a>' + ' Sticky Lips BBQ is right next door. Grab a pint, some BBQ and relax while, Cheers!';
 
-          $("#temp").html('Temp: ' + fTemp);
+          $("#temp").html('Temp: ' + myTrunc + '°F');
           $("#city").html('City: ' + city + ', NY');
           $("#main").html('Conditions: ' + weatherType);
 
-          if( fTemp > 70+'°F' ) {
+          if( fTemp > 70.0+'°F' ) {
             $("#brewery").html(Genesee);
             
-          } else if(fTemp > 65+'°F'){
+          } else if(fTemp > 65.0+'°F'){
             $("#brewery").html(RocBrewing);
 
-          } else if(fTemp > 60+'°F'){
+          } else if(fTemp > 60.0+'°F'){
             $("#brewery").html(threeHeads);
 
-          } else if(fTemp > 50+'°F'){
+          } else if(fTemp > 50.0+'°F'){
             $("#brewery").html(swiftWater);
           
-          } else if(fTemp > 40+'°F'){
+          } else if(fTemp > 40.0+'°F'){
             $("#brewery").html(lostBorough);
 
-          } else if(fTemp > 25+'°F'){
+          } else if(fTemp > 25.0+'°F'){
             $("#brewery").html(lock32);
 
-          } else if (fTemp > 1+'°F'){
+          } else if (myTrunc > 1.0){
             $("#brewery").html(Rohrbach);
           }   
       });
